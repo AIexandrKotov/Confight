bc5c87616fd48d881d949bc1244eb52fbdb291df
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2022-06-12 07:11:39 +0300

    upload

---
+++ BattleContext.cs
@@ -0,0 +1,229 @@
+﻿using KTXCore.Graphics;
+using KTXCore.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KTXCore;
+
+namespace Confight
+{
+    public enum Celltype
+    {
+        Field,
+        Player,
+
+        Enemy,
+        EnemyLazer,
+
+        Dynenemy,
+
+        Lazer,
+    }
+
+    public class BattleContext
+    {
+        public List<Spell> Spells = new List<Spell>();
+
+        public ConsoleColor GetColor(Celltype celltype)
+        {
+            switch (celltype)
+            {
+                case Celltype.Player: return ConsoleColor.White;
+                case Celltype.Field: return ConsoleColor.Black;
+                case Celltype.Enemy: return ConsoleColor.DarkRed;
+                case Celltype.EnemyLazer: return ConsoleColor.Red;
+                case Celltype.Lazer: return ConsoleColor.Magenta;
+                case Celltype.Dynenemy: return ConsoleColor.Green;
+            }
+            return ConsoleColor.Black;
+        }
+
+        public bool Death;
+
+        public int HMana = 100, CMana = 1000, Health = 100;
+        public int HManaMax = 1000, CManaMax = 1000, HealthMax = 1000;
+
+        public Celltype[,] BattleField;
+
+        public int PlayerPosition = 10;
+        public int PlayerHeight = 1;
+
+        public int MaxDown => BattleField.GetLength(1) - PlayerHeight;
+
+        public void MoveDown()
+        {
+            if (PlayerPosition < MaxDown) PlayerPosition++;
+        }
+
+        public void MoveUp()
+        {
+            if (PlayerPosition > 0) PlayerPosition--;
+        }
+
+        int MinigunLast = 0;
+        public void Minigun()
+        {
+            MinigunLast += 180;
+        }
+
+        public void AllLazer()
+        {
+            for (var i = 0; i < BattleField.GetLength(1); i++)
+            {
+                for (var j = 1; j <= 15; j++)
+                {
+                    BattleField[j, i] = Celltype.Lazer;
+                }
+            }
+        }
+
+        public void Lazer()
+        {
+            BattleField[1, PlayerPosition] = Celltype.Lazer;
+            BattleField[2, PlayerPosition] = Celltype.Lazer;
+            if (MinigunLast > 0) MinigunLast -= 1;
+        }
+
+        public void LongLazer()
+        {
+            BattleField[1, PlayerPosition] = Celltype.Lazer;
+            BattleField[2, PlayerPosition] = Celltype.Lazer;
+            BattleField[3, PlayerPosition] = Celltype.Lazer;
+            BattleField[4, PlayerPosition] = Celltype.Lazer;
+            BattleField[5, PlayerPosition] = Celltype.Lazer;
+            BattleField[6, PlayerPosition] = Celltype.Lazer;
+            BattleField[7, PlayerPosition] = Celltype.Lazer;
+        }
+
+        static Random rnd = new Random();
+        long ticks = 0;
+        int spawnrate = 120; //base 120
+        int dsprate = 300;
+        //int firerate = 60;
+        int dyns = 0;
+
+        public void Update()
+        {
+            if (MinigunLast > 0) Lazer();
+
+            ticks++;
+            HMana++;
+
+            if (ticks % spawnrate == 0)
+            {
+                if (spawnrate > 3) spawnrate -= 2;
+                BattleField[20 + rnd.Next(BattleField.GetLength(0)) - 20, rnd.Next(BattleField.GetLength(1))] = Celltype.Enemy;
+            }
+
+            if (spawnrate < 60)
+            {
+                if (ticks % dsprate == 0)
+                {
+                    dyns += 1;
+                    BattleField[50, 10] = Celltype.Dynenemy;
+                    if (dsprate > 60) dsprate -= 10;
+                    for (var j = 0; j < 10; j++)
+                    {
+                        for (var i = 1; i < 10; i++)
+                        {
+                            if (dyns > j * 10 + i - 1)
+                            {
+                                BattleField[50 - j, 10 - i] = Celltype.Dynenemy;
+                                BattleField[50 - j, 10] = Celltype.Dynenemy;
+                                BattleField[50 - j, 10 + i] = Celltype.Dynenemy;
+                            }
+                        }
+                    }
+                }
+            }
+
+            var width = BattleField.GetLength(0);
+            var height = BattleField.GetLength(1);
+            for (var i = 0; i < width; i++)
+                for (var j = 0; j < height; j++)
+                {
+                    if (BattleField[i, j] == Celltype.Dynenemy)
+                    {
+                        if (i > 1)
+                        {
+                            BattleField[i - 1, j] = Celltype.Dynenemy;
+                            BattleField[i, j] = Celltype.Field;
+                        }
+                        else Health -= 25;
+                    }
+
+                    if (BattleField[i, j] == Celltype.EnemyLazer)
+                    {
+                        if (i > 1)
+                        {
+                            switch (BattleField[i - 1, j])
+                            {
+                                case Celltype.Field:
+                                    BattleField[i - 1, j] = Celltype.EnemyLazer;
+                                    break;
+                                case Celltype.Lazer:
+                                    BattleField[i - 1, j] = Celltype.Field;
+                                    CMana += 2;
+                                    break;
+                            }
+                        }
+                        else if (j == PlayerPosition) CMana += 1;
+                        else Health -= 5;
+                        BattleField[i, j] = Celltype.Field;
+                    }
+
+                    if (BattleField[i, j] == Celltype.Enemy && i > 0)
+                    {
+                        if (rnd.Chance(0.01)) BattleField[i - 1, j] = Celltype.EnemyLazer;
+                    }
+                }
+
+            for (var i = width - 1; i >= 0; i--)
+            {
+                for (var j = height - 1; j >= 0; j--)
+                {
+                    if (BattleField[i, j] == Celltype.Lazer)
+                    {
+                        BattleField[i, j] = Celltype.Field;
+                        if (i + 1 < width)
+                        {
+                            if (BattleField[i + 1, j] == Celltype.Enemy) CMana += 10;
+                            BattleField[i + 1, j] = Celltype.Lazer;
+                        }
+                    }
+                }
+            }
+
+            for (var i = 0; i < BattleField.GetLength(1); i++)
+            {
+                if (i >= PlayerPosition && i < PlayerPosition + PlayerHeight) BattleField[0, i] = Celltype.Player;
+                else BattleField[0, i] = Celltype.Field;
+            }
+
+            if (HMana > HManaMax) HMana = HManaMax;
+            if (CMana > CManaMax) CMana = CManaMax;
+            if (Health > HealthMax) Health = HealthMax;
+            if (Health <= 0)
+            {
+                Death = true;
+                Health = 0;
+            }
+        }
+
+        public BattleContext()
+        {
+            BattleField = new Celltype[60, 20];
+            Spells.Add(new Spell(15, 0, 0, "(D) Lazer", Key.D, context => context.Lazer()));
+            Spells.Add(new Spell(15, 0, 0, "", 1, context => context.Lazer()));
+            Spells.Add(new Spell(70, 0, 0, "(F) Long Lazer", Key.F, context => context.LongLazer()));
+            Spells.Add(new Spell(100, 0, -35, "(Q) Heal", Key.Q, context => { }));
+
+            //Spells.Add(new Spell(-1000, 100, 0, "(X) Fill", Key.X, context => { }));
+            Spells.Add(new Spell(0, 100, -150, "(A) Cold Heal", Key.A, context => { }));
+            Spells.Add(new Spell(0, 150, 0, "(R) Minigun", Key.R, context => context.Minigun()));
+            Spells.Add(new Spell(0, 200, 0, "(G) Ultimate", Key.G, context => context.AllLazer()));
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KTXCore;
+using KTXCore.Extensions;
+using KTXCore.Graphics;
+using Console = KTXCore.Console;
+
+namespace Confight
+{
+    public abstract class SpellInfo
+    {
+        public int HManaCost { get; set; }
+        public int CManaCost { get; set; }
+        public int HealthCost { get; set; }
+        public string Title { get; set; }
+
+        public void Draw(int i, int j)
+        {
+            Console.SetCursorPosition(i, j);
+            Console.Write(Title);
+            if (HealthCost != 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($" +{-HealthCost}".Replace("+-", "-"));
+            }
+            if (HManaCost != 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write($" +{-HManaCost}".Replace("+-", "-"));
+            }
+            if (CManaCost != 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write($" +{-CManaCost}".Replace("+-", "-"));
+            }
+            Console.ResetFore();
+        }
+    }
+
+    public class Spell : SpellInfo
+    {
+        private Action<BattleContext> Invk;
+
+        public byte ActiveKey;
+
+        public Spell(int hm, int cm, int h, string title, byte key, Action<BattleContext> invoke)
+        {
+            (HManaCost, CManaCost, HealthCost, Title) = (hm, cm, h, title);
+            ActiveKey = key;
+            Invk = invoke;
+        }
+
+        public void Invoke(BattleContext context)
+        {
+            if (context.HMana >= HManaCost && context.CMana >= CManaCost && context.Health > HealthCost)
+            {
+                context.HMana -= HManaCost;
+                context.CMana -= CManaCost;
+                context.Health -= HealthCost;
+                Invk.Invoke(context);
+            }
+            if (context.HMana > context.HManaMax) context.HMana = context.HManaMax;
+            if (context.CMana > context.CManaMax) context.CMana = context.CManaMax;
+            if (context.Health > context.HealthMax) context.Health = context.HealthMax;
+        }
+    }
+
+    public class Endgame : Block
+    {
+        protected override ConsoleColor Background => ConsoleColor.Black;
+
+        public BattleContext Context;
+
+        public Endgame(BattleContext context)
+        {
+            Context = context;
+            OnAllRedraw += AllRedraw;
+            OnKeyPress += key => { if (key == Key.Spacebar) Close(); };
+        }
+
+        public void AllRedraw()
+        {
+            (Reference as Game).Context = new BattleContext();
+            var rect = new Rectangle(50, 20, Alignment.CenterWidth | Alignment.CenterHeight);
+            var (left, top) = rect.Draw();
+            Console.SetCursorPosition(left + 2, top + 2);
+            Console.Write("You are death!");
+            Console.SetCursorPosition(left + 2, top + rect.Height - 3);
+            Console.Write("Press SPACE to restart");
+        }
+    }
+
+    public class Game : Block
+    {
+        public BattleContext Context;
+
+        protected override ConsoleColor Background => ConsoleColor.Black;
+
+        public ConsoleColor[,] LastDrawed;
+
+        public bool Pause;
+
+        public Game()
+        {
+            Console.Title = "Confight 1.0";
+            Console.KeySplit = 200;
+            Console.KeyWait = 1000;
+            Console.OnlyOneRepeation = false;
+            Console.Theme = Theme.Dark;
+            Context = new BattleContext();
+            Add(() => Context.Death, () => Start(new Endgame(Context)));
+            Add(Up, Down);
+            Add(() => !Pause, UpdateField);
+            OnAllRedraw += AllRedraw;
+            OnKeyPress += KeyPress;
+        }
+
+        public void AllRedraw()
+        {
+            LastDrawed = null;
+            Console.SetCursorPosition(65, 7);
+            var current = 7;
+            foreach (var spell in Context.Spells)
+                if (!string.IsNullOrEmpty(spell.Title))
+                    spell.Draw(65, current++);
+            Console.ResetFore();
+        }
+
+        const int offset_top = 7;
+        const int offset_left = 1;
+
+        public void UpdateField()
+        {
+            Context.Update();
+            Console.SetCursorPosition(1, 1); Console.ForegroundColor = ConsoleColor.Red; Console.Write(Context.Health.ToString().PadRight(5));
+            Graph.Bar(6, 1, Context.Health / (double)Context.HealthMax, ConsoleColor.Red, ConsoleColor.DarkRed, Console.FixedWindowWidth - 7);
+            Console.SetCursorPosition(1, 2); Console.ForegroundColor = ConsoleColor.Magenta; Console.Write(Context.HMana.ToString().PadRight(5));
+            Graph.Bar(6, 2, Context.HMana / (double)Context.HManaMax, ConsoleColor.Magenta, ConsoleColor.DarkMagenta, Console.FixedWindowWidth - 7);
+            Console.SetCursorPosition(1, 3); Console.ForegroundColor = ConsoleColor.Cyan; Console.Write(Context.CMana.ToString().PadRight(5));
+            Graph.Bar(6, 3, Context.CMana / (double)Context.CManaMax, ConsoleColor.Cyan, ConsoleColor.DarkCyan, Console.FixedWindowWidth - 7);
+            if (LastDrawed == null)
+            {
+                LastDrawed = new ConsoleColor[Context.BattleField.GetLength(0), Context.BattleField.GetLength(1)];
+                for (var i = 0; i < Context.BattleField.GetLength(0); i++)
+                    for (var j = 0; j < Context.BattleField.GetLength(1); j++)
+                    {
+                        LastDrawed[i, j] = Context.GetColor(Context.BattleField[i, j]);
+                        Console.SetCursorPosition(offset_left + i, offset_top + j);
+                        Console.BackgroundColor = LastDrawed[i, j];
+                        Graph.OutSpaces(1);
+                    }
+            }
+            else
+            {
+                for (var i = 0; i < Context.BattleField.GetLength(0); i++)
+                    for (var j = 0; j < Context.BattleField.GetLength(1); j++)
+                    {
+                        var gc = Context.GetColor(Context.BattleField[i, j]);
+                        if (LastDrawed[i, j] != gc)
+                        {
+                            LastDrawed[i, j] = gc;
+                            Console.SetCursorPosition(offset_left + i, offset_top + j);
+                            Console.BackgroundColor = LastDrawed[i, j];
+                            Graph.OutSpaces(1);
+                        }
+                    }
+            }
+            Console.ResetColor();
+        }
+
+        static void Main() => new Game().Start();
+
+        void Up() => Context.MoveUp();
+        void Down() => Context.MoveDown();
+
+        void KeyPress(byte key)
+        {
+            var ind = Context.Spells.FindIndex(x => x.ActiveKey == key);
+            if (ind != -1) Context.Spells[ind].Invoke(Context);
+
+            KeypressInline(key, "Up", Key.W);
+            KeypressInline(key, "Down", Key.S);
+        }
+    }
+}
